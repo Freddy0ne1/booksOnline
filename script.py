d213e46af22ac092d3ec4423c7d7ec12150fb744
Freddy0ne1 <freddykhuti@gmail.com>
2025-03-14 17:57:35 +0100

    Modification du script

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-import requests
+import requests 
 from bs4 import BeautifulSoup
 import csv
 
@@ -7,32 +7,68 @@ url = "https://books.toscrape.com/catalogue/emma_17/index.html"
 
 # Lancer la requête HTTP
 reponse = requests.get(url)
-page = reponse.content
+soup = BeautifulSoup(reponse.text, "html.parser")
 
-soup = BeautifulSoup(page, "html.parser")
+# Ajout de l'URL du produit
+product_page_url_text = [url]
 
-# Extraire le titre
-titres = soup.find("h1")
-titre_textes = [titres.get_text(strip=True)] if titres else []
-for titre in titres:
-    titre_textes.append(titre.string)
+# Extraction category, title
+page = soup.find("div", class_="container-fluid page")
+categories = page.find("ul").find_all("li")[2].get_text(strip=True)
+category_text = [categories]
 
-# Extraire le prix
-prix = soup.find("p", class_="price_color")
-prix_textes = [prix.get_text(strip=True)] if prix else []
+titles = page.find("ul").find_all("li")[3]
+title_text = []
+for title in titles:
+    title_text.append(title.string)
+    
+# Extraction product_description
+product_descriptions = soup.find_all("p")[3]
+product_description_text = []
+for product_description in product_descriptions:
+    product_description_text.append(product_description.string)
 
-# Extraire la disponibilité
-disponibilites = soup.find("p", class_="instock availability")
-disponibilite_textes = [disponibilites.get_text(strip=True)] if disponibilites else []
+# Extraction upc prices number_available review_rating
+table = soup.find("table", class_="table table-striped")
+upc = table.find_all("td")[0]
+universal_product_code_text = []
+for universal_product_code in upc:
+    universal_product_code_text.append(universal_product_code.string)
 
-# Extraire la description
-descriptions = soup.find("div", id="product_description")
-description = descriptions.find_next_sibling("p").text.strip()
+i_taxes = table.find_all("td")[3]
+price_including_tax_text = []
+for price_including_tax in i_taxes:
+    price_including_tax_text.append(price_including_tax.string)
 
-# Edition du fichier CSV
-en_tete = ["titre", "prix", "disponibilité", "description"]
+e_taxes = table.find_all("td")[2]
+price_excluding_tax_text = []
+for price_excluding_tax in e_taxes:
+    price_excluding_tax_text.append(price_excluding_tax.string)
+
+num_availables = table.find_all("td")[5]
+number_available_text = []
+for number_available in num_availables:
+    number_available_text.append(number_available.string)
+
+r_ratings = table.find_all("td")[6]
+review_rating_text = []
+for review_rating in r_ratings:
+    review_rating_text.append(review_rating.string)
+
+# Extraction image
+image = soup.find("div", class_="item active")
+image_url_text = []
+
+if image:
+    image_tag = image.find("img")
+    if image_tag and "src" in image_tag.attrs:
+        image_url = image_tag["src"]  # On récupère directement l'URL
+        image_url_text.append(image_url)
+
+# Ecrire dans le fichier CSV
+en_tete = ["product_page_url", "universal_product_code", "title", "price_including_tax", "price_excluding_tax", "number_available", "product_description", "category", "review_rating", "image_url"]
 with open("../data.csv", "w", newline="") as csv_file:
     writer = csv.writer(csv_file, delimiter=",")
     writer.writerow(en_tete)
-    for titre, prix, disponibilite, in zip(titre_textes, prix_textes, disponibilite_textes):
-        writer.writerow([titre, prix, disponibilite, description])
+    for product_page_url, universal_product_code, title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url in zip(product_page_url_text, universal_product_code_text, title_text, price_including_tax_text, price_excluding_tax_text, number_available_text, product_description_text, category_text, review_rating_text, image_url_text):
+        writer.writerow([product_page_url, universal_product_code, title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url])
